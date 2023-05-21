a50c3d56670a560ec065ec6b9c666ccb5db359c5
Manish Lama <lmanish931@gmail.com>
2023-05-21 18:35:35 +0545

    trying

---
+++ _next/static/chunks/app/page-ec78143af0408c22.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{7656:function(e,n,s){Promise.resolve().then(s.t.bind(s,2011,23)),Promise.resolve().then(s.t.bind(s,414,23))}},function(e){e.O(0,[382,667,139,744],function(){return e(e.s=7656)}),_N_E=e.O()}]);
